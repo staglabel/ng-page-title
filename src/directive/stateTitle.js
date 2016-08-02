9545b81fc63c62dc02af80d7d3395bbac690989a
Simon <simon@simonemms.com>
2016-08-02 20:34:37 +0100

    The named-views support broke the default support. This is the same a v1.1.1

--- src/directive/stateTitle.js
+++ src/directive/stateTitle.js
@@ -28,43 +28,16 @@ function StateTitle ($rootScope, $interpolate, $state) {
                 var title = attrs.stateTitle || "Untitled page"; /* Get the default title */
                 var titleElement = attrs.titleElement || "pageTitle"; /* Where to look for the title in the data */
                 var pattern = attrs.pattern || null; /* Do we need to decorate the title? */
-                var currentState = $state.$current; /* Create reference to the current state */
-                var viewWithTitle = "";
 
-                /* Check for multiple views on the state */
-                if (_.has(toState, "views")) {
-
-                    /* Find the name of first view found with a titleElement */
-                    viewWithTitle = _.findKey(toState.views, function (view) {
-                        if (_.has(view, "data." + titleElement)) {
-                            return view;
-                        }
-                    });
-
-                    title = toState.views[viewWithTitle].data[titleElement];
-
-                } else {
-
-                    /* Get the page title from the data element */
-                    if (_.has(toState, "data." + titleElement) && _.isEmpty(toState.data[titleElement]) === false) {
-                        title = toState.data[titleElement];
-                    }
-
-                }
-
-                /* Build the name of the scope we should target on locals */
-                var localsName;
-
-                // If this is a dot separated name
-                if (currentState.self.name.lastIndexOf(".") >= 0) {
-                    localsName = viewWithTitle + "@" + currentState.self.name.slice(0, currentState.self.name.lastIndexOf("."));
-                } else {
-                    localsName = viewWithTitle + "@" + currentState.self.name;
+                /* Get the page title from the data element */
+                if (_.has(toState, "data") && _.has(toState.data, titleElement) && _.isEmpty(toState.data[titleElement]) === false) {
+                    title = toState.data[titleElement];
                 }
 
                 /* Interpolate the title */
-                if (_.has(currentState, "locals") && _.has(currentState.locals, localsName)) {
-                    currentState = currentState.locals[localsName];
+                var currentState = $state.$current;
+                if (_.has(currentState, "locals") && _.has(currentState.locals, "globals")) {
+                    currentState = currentState.locals.globals;
                 }
 
                 title = $interpolate(title)(currentState);
@@ -92,4 +65,4 @@ function StateTitle ($rootScope, $interpolate, $state) {
 }
 
 
-module.exports = ["$rootScope", "$interpolate", "$state", StateTitle];
+module.exports = StateTitle;
