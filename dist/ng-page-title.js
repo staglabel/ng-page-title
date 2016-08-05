05155637201ba371f3e2dde22b8496503208a323
Simon <simon@simonemms.com>
2016-08-05 17:34:33 +0100

    v1.3.1 build

--- dist/ng-page-title.js
+++ dist/ng-page-title.js
@@ -2,10 +2,10 @@
  * ng-page-title
  *
  * @author Simon Emms <simon@simonemms.com>
- * @build 2016-08-02T20:52:23
+ * @build 2016-08-05T17:34:26
  * @description Page title directive for an Angular project
  * @license MIT
- * @version v1.3.0
+ * @version v1.3.1
  */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
